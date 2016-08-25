240df99aeedf7ddf055eb9c3a92492b0d24d3a0e
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-08-25 21:13:53 +0300

    I/O chapter 1

---
+++ 1/begin1.go
@@ -0,0 +1,18 @@
+// Дана сторона квадрата a. Найти его периметр P = 4·a.
+package main
+
+import "fmt"
+
+func main() {
+	var x float64
+	fmt.Println("Необходимо найти периметр квадрата")
+	x = number("Введите значение стороны:")
+    fmt.Println("Периметр равен: ", 4*x)
+}
+
+func number(msg string) float64 {
+	fmt.Print(msg + " > ")
+	var num float64
+	fmt.Scan(&num)
+	return num
+}
