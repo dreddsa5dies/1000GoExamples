dc13c526fa7416f301a409586e3ef360dfc5317b
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-08-27 12:14:09 +0300

    18

--- IOBegin/018/begin.go
+++ IOBegin/018/begin.go
@@ -1,20 +1,36 @@
-// Дана длина L окружности. Найти ее радиус R и площадь S круга,
-// ограниченного этой окружностью, учитывая, что L = 2· π · R , S = π · R^2 . В ка-
-// честве значения π использовать 3.14.
+// Даны три точки A , B , C на числовой оси. Точка C расположена между
+// точками A и B . Найти произведение длин отрезков AC и BC
 package main
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/dreddsa5dies/1000GoExamples/util"
 )
 
 func main() {
-	var x, y float64
-	const pi2 = 2 * math.Pi
-	x = util.NoNNumber("Введите длину окружности")
-	y = x / pi2
-	fmt.Println("Радиус круга:\t", y)
-	fmt.Println("Площадь круга:\t", y*y*math.Pi)
+	var x, y, z float64
+	x = util.Number("Введите точку А")
+	y = util.Number("Введите точку B")
+	if x < y {
+		z = util.Number("Введите точку C")
+		for z < x || z > y {
+			fmt.Println("Ввод неверных данных, введите C меньше В и больше А")
+			_, err := fmt.Scanf("%f", &z)
+			if err != nil {
+				panic("Ввод неверных данных")
+			}
+		}
+	} else {
+		z = util.Number("Введите точку C")
+		for z > x || z < y {
+			fmt.Println("Ввод неверных данных, введите C меньше A и больше B")
+			_, err := fmt.Scanf("%f", &z)
+			if err != nil {
+				panic("Ввод неверных данных")
+			}
+		}
+	}
+	sum := util.ModNumber(x-z) * util.ModNumber(y-z)
+	fmt.Println("произведение длинн отрезков:\t", sum)
 }
