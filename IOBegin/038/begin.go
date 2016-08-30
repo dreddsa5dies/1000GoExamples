4f6cda5e5b38cb80b3109824f33a41b416a513fe
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-08-30 21:34:49 +0300

    34-40, IObegin complete

--- IOBegin/038/begin.go
+++ IOBegin/038/begin.go
@@ -1,20 +1,18 @@
-// Дана длина L окружности. Найти ее радиус R и площадь S круга,
-// ограниченного этой окружностью, учитывая, что L = 2· π · R , S = π · R^2 . В ка-
-// честве значения π использовать 3.14.
+// Решить линейное уравнение A·x + B = 0, заданное своими коэффици-
+// ентами A и B (коэффициент A не равен 0)
 package main
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/dreddsa5dies/1000GoExamples/util"
 )
 
 func main() {
 	var x, y float64
-	const pi2 = 2 * math.Pi
-	x = util.NoNNumber("Введите длину окружности")
-	y = x / pi2
-	fmt.Println("Радиус круга:\t", y)
-	fmt.Println("Площадь круга:\t", y*y*math.Pi)
+	fmt.Println("A·x + B = 0")
+	x = util.NotNullNumber("Введите A")
+	y = util.Number("Введите B")
+	z := (0 - y) / x
+	fmt.Println("x = ", z)
 }
--- IOBegin/040/begin.go
+++ IOBegin/040/begin.go
@@ -1,20 +1,31 @@
-// Дана длина L окружности. Найти ее радиус R и площадь S круга,
-// ограниченного этой окружностью, учитывая, что L = 2· π · R , S = π · R^2 . В ка-
-// честве значения π использовать 3.14.
+// Найти решение системы линейных уравнений вида
+// A 1 · x + B 1 · y = C 1 ,
+// A 2 · x + B 2 · y = C 2 ,
+// заданной своими коэффициентами A 1 , B 1 , C 1 , A 2 , B 2 , C 2 , если известно, что
+// данная система имеет единственное решение. Воспользоваться формулами
+// x = ( C 1 · B 2 – C 2 · B 1 )/ D ,
+// y = ( A 1 · C 2 – A 2 · C 1 )/ D ,
+// где D = A 1 · B 2 – A 2 · B 1
 package main
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/dreddsa5dies/1000GoExamples/util"
 )
 
 func main() {
-	var x, y float64
-	const pi2 = 2 * math.Pi
-	x = util.NoNNumber("Введите длину окружности")
-	y = x / pi2
-	fmt.Println("Радиус круга:\t", y)
-	fmt.Println("Площадь круга:\t", y*y*math.Pi)
+	var a1, a2, b1, b2, c1, c2, d, x, y float64
+	fmt.Println("\tA1 · x + B1 · y = C1\n\tA2 · x + B2 · y = C2")
+	a1 = util.Number("Введите A1")
+	b1 = util.Number("Введите B1")
+	c1 = util.Number("Введите C1")
+	a2 = util.Number("Введите A2")
+	b2 = util.Number("Введите B2")
+	c2 = util.Number("Введите C2")
+	d = a1*b2 - a2*b1
+	x = (c1*b2 - c2*b1) / d
+	y = (a1*c2 - a2*c1) / d
+	fmt.Println("x = ", x)
+	fmt.Println("y = ", y)
 }
