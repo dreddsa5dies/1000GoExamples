ed3a297c048b263b2b6b5f7e0d5e5207c0d806e4
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-01 15:19:40 +0300

    14

--- integer/014/integer.go
+++ integer/014/integer.go
@@ -1,7 +1,17 @@
+// Дано трехзначное число. В нем зачеркнули первую справа цифру и
+// приписали ее слева. Вывести полученное число
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var x uint
+	for x < 100 || x > 1000 {
+		x = util.UInteger("Введите трехзначное число A")
+	}
+	fmt.Printf("полученное число: %v\n", (x%10)*100+10*(x/100)+(x-(100*(x/100)))/10)
 }
