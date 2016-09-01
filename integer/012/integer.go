2436c2196c9a910817715a27a82e730423ae22c9
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-01 14:03:06 +0300

    12 complete

--- integer/012/integer.go
+++ integer/012/integer.go
@@ -1,7 +1,17 @@
+// Дано трехзначное число. Вывести число, полученное при прочтении
+// исходного числа справа налево
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var x uint
+	for x < 100 || x > 1000 {
+		x = util.UInteger("Введите трехзначное число A")
+	}
+	fmt.Printf("число справа налево: %v\n", (x%10)*100+10*((x-(100*(x/100)))/10)+x/100)
 }
