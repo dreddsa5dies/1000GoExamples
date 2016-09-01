434b58d1623e6243be497aad165aa04874ba38fe
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-01 13:54:45 +0300

    8 & 10 complete

--- integer/010/integer.go
+++ integer/010/integer.go
@@ -1,7 +1,18 @@
+// ДДано трехзначное число. Вывести вначале его последнюю цифру
+// (единицы), а затем — его среднюю цифру (десятки)
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var x uint
+	for x < 100 || x > 1000 {
+		x = util.UInteger("Введите трехзначное число A")
+	}
+	fmt.Printf("его последняя цифра (единицы): %v\n", x%10)
+	fmt.Printf("его средняя цифра (десятки): %v\n", (x-(100*(x/100)))/10)
 }
