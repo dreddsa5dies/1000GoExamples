0c747575b73e84f9b5fcafbc4f9caa66553569c4
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-01 13:50:28 +0300

    9 complete

--- integer/009/integer.go
+++ integer/009/integer.go
@@ -1,7 +1,17 @@
+// Дано трехзначное число. Используя одну операцию деления нацело,
+// вывести первую цифру данного числа (сотни)
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var x uint
+	for x < 100 || x > 1000 {
+		x = util.UInteger("Введите трехзначное число A")
+	}
+	fmt.Printf("первая цифра данного числа (сотни): %v\n", x/100)
 }
