090852820926da9d745b1fef1990ce4b4eea7988
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-01 15:27:01 +0300

    15, 16, 17 complete

--- integer/015/integer.go
+++ integer/015/integer.go
@@ -1,7 +1,18 @@
+// Дано трехзначное число. Вывести число, полученное при переста-
+// новке цифр сотен и десятков исходного числа (например, 123 перейдет
+// в 213)
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var x uint
+	for x < 100 || x > 1000 {
+		x = util.UInteger("Введите трехзначное число A")
+	}
+	fmt.Printf("полученное число: %v\n", 100*((x-(100*(x/100)))/10)+10*(x/100)+x%10)
 }
--- integer/017/integer.go
+++ integer/017/integer.go
@@ -1,7 +1,20 @@
+// Дано целое число, большее 999. Используя одну операцию деления
+// нацело и одну операцию взятия остатка от деления, найти цифру, соответ-
+// ствующую разряду сотен в записи этого числа
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var x uint
+	for x < 999 {
+		x = util.UInteger("Введите целое число, большее 999")
+	}
+	x = x / 100
+	x = x % 10
+	fmt.Printf("разряд сотен: %v\n", x)
 }
