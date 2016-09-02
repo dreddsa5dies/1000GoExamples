80b7ac6f6e4ea41416ba8825b0c154c91e414723
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-02 09:44:28 +0300

    6-10 complete

---
+++ boolean/007/boolean.go
@@ -0,0 +1,17 @@
+// Даны три целых числа: A , B , C . Проверить истинность высказыва-
+// ния: «Число B находится между числами A и C »
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var a, b, c int
+	a = util.Integer("целое число A")
+	b = util.Integer("целое число B")
+	c = util.Integer("целое число C")
+	fmt.Println(a < b && b < c)
+}
---
+++ boolean/008/boolean.go
@@ -0,0 +1,16 @@
+// Даны два целых числа: A , B . Проверить истинность высказывания:
+// «Каждое из чисел A и B нечетное»
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var a, b int
+	a = util.Integer("целое число A")
+	b = util.Integer("целое число B")
+	fmt.Println(a%2 != 0 && b%2 != 0)
+}
