64aeaac664803dbd8bba988c0774efd078a93f0f
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-03 09:22:48 +0300

    11-14

---
+++ boolean/013/boolean.go
@@ -0,0 +1,17 @@
+// Даны три целых числа: A , B , C . Проверить истинность высказыва-
+// ния: «Хотя бы одно из чисел A , B , C положительное»
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var a, b, c int
+	a = util.Integer("целое число A")
+	b = util.Integer("целое число B")
+	c = util.Integer("целое число C")
+	fmt.Println(a > 0 || b > 0 || c > 0)
+}
