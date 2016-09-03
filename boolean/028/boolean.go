7eecdf33b047c069e4d194e1f8b57ee7a08a7bec
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-03 13:43:08 +0300

    27-28

---
+++ boolean/028/boolean.go
@@ -0,0 +1,19 @@
+// Даны числа x , y . Проверить истинность высказывания: «Точка с ко-
+// ординатами ( x , y ) лежит в первой или третьей координатной четверти»
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var x, y float64
+	var a, b bool
+	x = util.Number("число x")
+	y = util.Number("число y")
+	a = (y > 0) && (x > 0)
+	b = (y < 0) && (x < 0)
+	fmt.Println(a || b)
+}
