570d4930cbc439bd7422d21fcbdd192cdbaeede8
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-05 15:59:07 +0300

    boolean complete

---
+++ boolean/037/boolean.go
@@ -0,0 +1,29 @@
+// Даны координаты двух различных полей шахматной доски x 1 , y 1 ,
+// x 2 , y 2 (целые числа, лежащие в диапазоне 1–8). Проверить истинность вы-
+// сказывания: «Король за один ход может перейти с одного поля на другое»
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var x1, x2, y1, y2 uint
+	var a bool
+	for x1 < 1 || x1 > 8 {
+		x1 = util.UInteger("число x1")
+	}
+	for y1 < 1 || y1 > 8 {
+		y1 = util.UInteger("число y1")
+	}
+	for x2 < 1 || x2 > 8 {
+		x2 = util.UInteger("число x2")
+	}
+	for y2 < 1 || y2 > 8 {
+		y2 = util.UInteger("число y2")
+	}
+	a = ((x2 == (x1 - 1)) || (x2 == (x1 + 1))) || (y2 == (y1 - 1)) || (y2 == (y1 + 1))
+	fmt.Println(a)
+}
---
+++ boolean/038/boolean.go
@@ -0,0 +1,29 @@
+// Даны координаты двух различных полей шахматной доски x 1 , y 1 ,
+// x 2 , y 2 (целые числа, лежащие в диапазоне 1–8). Проверить истинность вы-
+// сказывания: «Слон за один ход может перейти с одного поля на другое»
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var x1, x2, y1, y2 int
+	var a bool
+	for x1 < 1 || x1 > 8 {
+		x1 = util.Integer("число x1")
+	}
+	for y1 < 1 || y1 > 8 {
+		y1 = util.Integer("число y1")
+	}
+	for x2 < 1 || x2 > 8 {
+		x2 = util.Integer("число x2")
+	}
+	for y2 < 1 || y2 > 8 {
+		y2 = util.Integer("число y2")
+	}
+	a = util.ModNumber(float64(x2-x1)) == util.ModNumber(float64(y2-y1))
+	fmt.Println(a)
+}
