55d39c4873b7d65ce14389f251fedb60f71f2fc7
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-05 15:11:46 +0300

    34, 35

---
+++ boolean/035/boolean.go
@@ -0,0 +1,30 @@
+// Даны координаты двух различных полей шахматной доски x 1 , y 1 ,
+// x 2 , y 2 (целые числа, лежащие в диапазоне 1–8). Проверить истинность вы-
+// сказывания: «Данные поля имеют одинаковый цвет»
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var x1, x2, y1, y2 uint
+	var a, b bool
+	for x1 < 1 || x1 > 8 {
+		x1 = util.UInteger("число x1")
+	}
+	for y1 < 1 || y1 > 8 {
+		y1 = util.UInteger("число y1")
+	}
+	for x2 < 1 || x2 > 8 {
+		x2 = util.UInteger("число x2")
+	}
+	for y2 < 1 || y2 > 8 {
+		y2 = util.UInteger("число y2")
+	}
+	a = ((x1%2 == 0) && (y1%2 == 0)) || ((x1%2 != 0) && (y1%2 != 0))
+	b = ((x2%2 == 0) && (y2%2 == 0)) || ((x2%2 != 0) && (y2%2 != 0))
+	fmt.Println(a == b)
+}
