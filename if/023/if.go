3af0b31404c1e3f6c5dc037b3da0b55ab6d40386
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-06 16:53:03 +0300

    23 надо доделать

--- if/023/if.go
+++ if/023/if.go
@@ -10,17 +10,33 @@ import (
 )
 
 func main() {
-	var x1, y1, x2, y2, x3, y3 int
+	var x1, y1, x2, y3, x4, y4 int
 	x1 = util.Integer("X1")
 	y1 = util.Integer("Y1")
 
-	x2 = util.Integer("X2")
-	y2 = util.Integer("Y2")
-
-	x3 = util.Integer("X3")
-	y3 = util.Integer("Y4")
-
-	if x1 > 0 && y1 > 0 {
-		fmt.Printf(": %v\n", 1)
+	x2 = x1
+	for x1 == x2 {
+		x2 = util.Integer("X2")
 	}
+	fmt.Printf("Y2: %v\n", y1)
+
+	fmt.Printf("X3: %v\n", x2)
+	y3 = y1
+	for y3 == y1 {
+		y3 = util.Integer("Y3")
+	}
+
+	if y3 < 0 {
+		x4 = x1
+	} else {
+		x4 = -x1
+	}
+
+	if x1 < 0 {
+		y4 = -y3
+	} else {
+		y4 = y3
+	}
+
+	fmt.Printf("X4: %v\tY4: %v\n", x4, y4)
 }
