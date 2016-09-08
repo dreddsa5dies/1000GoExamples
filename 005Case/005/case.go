3e5259b956a12581dbe2c3e50231313367a27338
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-08 09:20:55 +0300

    5

---
+++ 005Case/005/case.go
@@ -0,0 +1,35 @@
+// Арифметические действия над числами пронумерованы следующим
+// образом: 1 — сложение, 2 — вычитание, 3 — умножение, 4 — деление.
+// Дан номер действия N (целое число в диапазоне 1–4) и вещественные чис-
+// ла A и B ( В не равно 0). Выполнить над числами указанное действие и вы-
+// вести результат
+package main
+
+import (
+	"fmt"
+
+	"github.com/dreddsa5dies/1000GoExamples/util"
+)
+
+func main() {
+	var x, y, z uint
+	for x < 1 || x > 4 {
+		x = util.UInteger("действие")
+	}
+
+	z = util.UInteger("A")
+	for y == 0 {
+		y = util.UInteger("B")
+	}
+
+	switch {
+	case x == 1:
+		fmt.Printf("%v\n", z+y)
+	case x == 2:
+		fmt.Printf("%v\n", z-y)
+	case x == 3:
+		fmt.Printf("%v\n", z*y)
+	case x == 4:
+		fmt.Printf("%v\n", z/y)
+	}
+}
