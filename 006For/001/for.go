3d0c277bce60980571cc2cca7d0631d91c4fe0a9
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-14 11:17:08 +0300

    1 complete

--- 006For/001/for.go
+++ 006For/001/for.go
@@ -1,3 +1,4 @@
+// Даны целые числа K и N ( N > 0). Вывести N раз число K
 package main
 
 import (
@@ -7,6 +8,19 @@ import (
 )
 
 func main() {
-	m := ioutil.Integer("месяц")
-	fmt.Println(m)
+	var m, n int
+
+	m = ioutil.Integer("число")
+	n = -1
+	for n < 0 {
+		n = ioutil.Integer("число")
+	}
+
+	loop(m, n)
+}
+
+func loop(m, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Printf("%v\n", m)
+	}
 }
