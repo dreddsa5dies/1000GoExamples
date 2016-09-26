115e71538de74612aa097bee9dc65a932ddb7200
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-26 13:14:57 +0300

    8 change, 9 ok +t +b

---
+++ 007While/009/while_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestCountLoop(t *testing.T) {
+	var tests1 = []struct {
+		inputA int
+		want   int
+	}{
+		{3, 2},
+		{7, 2},
+		{9, 3},
+	}
+	for _, test := range tests1 {
+		if got := countLoop(test.inputA); got != test.want {
+			t.Errorf("countLoop(%v) = %v", test.inputA, got)
+		}
+	}
+}
+
+func BenchmarkCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		countLoop(8)
+	}
+}
