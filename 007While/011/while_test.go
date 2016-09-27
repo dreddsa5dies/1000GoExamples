500fdfe21df8a21fba70178ce8377439ececc2a9
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-09-27 09:11:47 +0300

    11 ok +t +b

---
+++ 007While/011/while_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestCountLoop(t *testing.T) {
+	var tests = []struct {
+		inputN  int
+		wantK   int
+		wantSum int
+	}{
+		{3, 3, 2},
+		{7, 10, 4},
+		{9, 10, 4},
+	}
+	for _, test := range tests {
+		if gotK, gotSum := countLoop(test.inputN); gotK != test.wantK && gotSum != test.wantSum {
+			t.Errorf("countLoop(%v) = %v, %v", test.inputN, gotK, gotSum)
+		}
+	}
+}
+
+func BenchmarkCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		countLoop(8)
+	}
+}
