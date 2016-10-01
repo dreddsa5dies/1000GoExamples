665e5152001b2ba162fd2f5dc5e831a69d0c907b
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-10-01 10:36:13 +0300

    8 + math/rand +b

--- 008Series/005/series.go
+++ 008Series/005/series.go
@@ -7,28 +7,27 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/dreddsa5dies/1000GoExamples/ioutil"
 )
 
 func main() {
-	var (
-		sum int
-		r   float64
-	)
 	n := ioutil.Integer("число N")
-
-	for i := 0; i < n; i++ {
-		r = ioutil.NoNNumber("число")
-		fmt.Printf("целая часть числа %v\n", int(r))
-		sum = sum + series(r)
-	}
-
+	sum := series(n)
 	fmt.Printf("Сумма: %v\n", sum)
 }
 
 // возврат суммы sum + int(r)
-func series(r float64) (sum int) {
-	sum = sum + int(r)
+func series(n int) (sum int) {
+	var r float64
+	for i := 0; i < n; i++ {
+		// генерация рандомного чисел floats в
+		// диапазоне `f < 10.0`.
+		r = (rand.Float64() * 5) + 5
+		fmt.Printf("число %v\n", r)
+		fmt.Printf("целая часть числа %v\n", int(r))
+		sum = sum + int(r)
+	}
 	return sum
 }
