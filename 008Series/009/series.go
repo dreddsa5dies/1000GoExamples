74ea817d47717c7b0471cf13781529c3e2454338
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-10-03 10:02:35 +0300

    9 ok +b

---
+++ 008Series/009/series.go
@@ -0,0 +1,31 @@
+// Дано целое число N и набор из N целых чисел. Вывести в том же по-
+// рядке номера всех нечетных чисел из данного набора и количество K таких
+// чисел
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/dreddsa5dies/1000GoExamples/ioutil"
+)
+
+func main() {
+	n := ioutil.Integer("число N")
+	count := series(n)
+	fmt.Printf("Количество чисел: %v\n", count)
+}
+
+// возврат суммы sum + int(r)
+func series(n int) int {
+	var r, count int
+	for i := 0; i < n; i++ {
+		r = rand.Intn(n)
+		if r%2 == 1 {
+			count++
+			fmt.Printf("нечетное число: %v\n", r)
+		}
+	}
+	return count
+}
